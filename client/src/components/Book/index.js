1f3d6073f63c828da977ef8a9d7ddfc9db03c607
Scott G <glover.scott92@gmail.com>
2020-11-05 18:18:02 -0800

    books

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -13,8 +13,29 @@ function Book({ title, subtitle, authors, link, description, image, Button }) {
                 </Col>
                 <Col size="md-4">
                 </Col>
+                <Col size="md-4">
+                    <div className="btn-container">
+                        <a className="btn btn-light" target="_blank noreferrer" href={link}> View</a>
+                        <Button />
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col size="md-6">
+                    <p className="font-italic small"> By: {authors}</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col size="12 sm-4 md-2">
+                    <img className="img-thumbnail img-fluid w-100" src={image} alt={title} />
+                </Col>
+                <Col size="12 sm-8 md-10">
+                    <p>{description}</p>
+                </Col>
             </Row>
         </ListItem>
-    )
+    );
 
-}   
+}
+
+export default Book;
